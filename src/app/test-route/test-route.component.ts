7923ac2f287f374961beb34e9c8e362c392e9c69
Misha <Misha.Khohlov@yandex.ua>
2017-02-13 19:08:07 +0300

    maybe fixed error related to with eslint

--- src/app/test-route/test-route.component.ts
+++ src/app/test-route/test-route.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -7,23 +7,28 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './test-route.component.html',
   styleUrls: ['./test-route.component.scss']
 })
-export class TestRouteComponent implements OnDestroy {
+export class TestRouteComponent implements OnDestroy, OnInit {
   id: number;
   product: string;
   price: number;
   openLinkProps = false;
+  routeSubscription: Subscription;
+  querySubscription: Subscription;
 
   constructor(private activateRoute: ActivatedRoute) {
     // this.id = activateRoute.snapshot.params['id'];
+    this.routeSubscription = this.activateRoute.params.subscribe( params => this.id = params['id']);
+    this.querySubscription = this.activateRoute.queryParams.subscribe(
+      (queryParam: any) => {
+        this.product = queryParam['product'];
+        this.price = queryParam['price'];
+      }
+    );
   }
 
-  private routeSubscription: Subscription = this.activateRoute.params.subscribe( params => this.id = params['id']);
-  private querySubscription: Subscription = this.activateRoute.queryParams.subscribe(
-    (queryParam: any) => {
-      this.product = queryParam['product'];
-      this.price = queryParam['price'];
-    }
-  );
+  ngOnInit() {
+
+  }
 
   ngOnDestroy() {
     this.routeSubscription.unsubscribe();
