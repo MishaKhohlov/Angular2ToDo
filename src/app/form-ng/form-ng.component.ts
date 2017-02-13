024d22366dd8dd5ab1d8e08925ce84ebacae5e18
Misha <Misha.Khohlov@yandex.ua>
2017-02-13 12:37:00 +0300

    form builder add

--- src/app/form-ng/form-ng.component.ts
+++ src/app/form-ng/form-ng.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import {
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'my-form-ng',
@@ -10,26 +17,23 @@ export class FormNgComponent {
   myForm: FormGroup;
   phones = [];
 
-  constructor() {
-    this.myForm = new FormGroup({
-      'userName': new FormControl('Tom', [
-        Validators.required,
-        this.userNameValidator
-      ]),
-      'userEmail': new FormControl('', [
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = formBuilder.group({
+      'userName': ['Tom', [Validators.required, this.userNameValidator]],
+      'userEmail': ['', [
         Validators.required,
         Validators.pattern('[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}')
-      ]),
-      'userPhone': new FormControl('', [this.userPhoneValidator]),
-      'phones': new FormArray([
-        new FormControl("+7", Validators.required)
+      ]],
+      'userPhone': ['', [this.userPhoneValidator]],
+      'phones': formBuilder.array([
+        ['+7', Validators.required]
       ])
     });
-    console.log(this.myForm);
+
   }
 
-  addPhone(){
-    (<FormArray>this.myForm.controls["phones"]).push(new FormControl("+8", Validators.required));
+  addPhone() {
+    (<FormArray>this.myForm.controls['phones']).push(new FormControl('+8', [Validators.required, this.userPhoneValidator]));
   }
 
   userNameValidator(control: FormControl) {
@@ -43,7 +47,7 @@ export class FormNgComponent {
 
     console.log(control.value);
     if (control.value === '123') {
-      return {'userName': true};
+      return {'userPhone': true};
     }
     return null;
   }
